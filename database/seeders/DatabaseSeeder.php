69b8a5816f9ee36709de31818ce43963d948f17f
Jérémy <asjeremy83@gmail.com>
2024-06-25 14:53:57 -0400

    create fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Bien;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
          User::factory(10)->create();
          Category::factory(10)->create();
+         Bien::factory(100)->create();
+
 
         // User::factory()->create([
         //     'name' => 'Test User',
